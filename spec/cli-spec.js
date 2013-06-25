0e53a9e4012666acc0650b61800f513c4eabe0ee
Matt Hernandez <matt@modulus.io>
2013-06-24 20:56:07 -0400

    Stubbed commands and created tests for each.

--- spec/cli-spec.js
+++ spec/cli-spec.js
@@ -11,23 +11,57 @@ describe('cli', function() {
 
   describe('#test', function() {
 
-    it('should succeed with no path', function(done) {
+    it('`all` should call test with correct test type', function(done) {
       spyOn(dash, 'test');
 
-      cli.parse(['node', 'test', 'test']);
+      cli.parse(['node', 'test', 'test all']);
 
       expect(dash.test).wasCalled();
+      expect(dash.test).wasCalledWith('all', '');
 
       done();
     });
 
-    it('should accept a path', function(done) {
+    xit('`all` with filename should provide a path', function(done) {
       spyOn(dash, 'test');
 
-      cli.parse(['node', 'test', 'test', './path/to/addon']);
+      cli.parse(['node', 'test', '--filename=./path/to/addon-manifest.json', 'test all']);
 
       expect(dash.test).wasCalled();
-      expect(dash.test).wasCalledWith('./path/to/addon');
+      expect(cli.filename).toEqual('./path/to/addon-manifest.json');
+      expect(dash.test).wasCalledWith('all', './path/to/addon-manifest.json');
+
+      done();
+    });
+
+    it('`provision` should call test with correct test type', function(done) {
+      spyOn(dash, 'test');
+
+      cli.parse(['node', 'test', 'test provision']);
+
+      expect(dash.test).wasCalled();
+      expect(dash.test).wasCalledWith('provision', '', undefined);
+
+      done();
+    });
+
+    it('`deprovision` should call with correct test type', function(done) {
+      spyOn(dash, 'test');
+
+      cli.parse(['node', 'test', 'test deprovision', 0]);
+
+      expect(dash.test).wasCalled();
+      expect(dash.test).wasCalledWith('deprovision', '', 0);
+
+      done();
+    });
+
+    it('should call init', function(done) {
+      spyOn(dash, 'init');
+
+      cli.parse(['node', 'test', 'init']);
+
+      expect(dash.init).wasCalled();
 
       done();
     });
